096f464e05beb141395673012aaf9ec1dbf7d59e
BelusKirill <belus@vikab.by>
2022-10-05 10:47:10 +0300

    перенесена логина событий в action, добавлена отбратотка края карты

--- actions.py
+++ actions.py
@@ -1,9 +1,24 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING: # Получать доступ к конкретному типу
+    from engine import Engine
+    from entity import Entity
+
+
 class Action:
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        """
+        engine - это область, в которой выполняется это действие.
+        entity - это объект, выполняющий действие.
+        TЭтот метод должен быть переопределен подклассами Action.
+        """
+        raise NotImplementedError()
 
 
 class EscapeAction(Action):
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
 
 
 class MovementAction(Action):
@@ -12,3 +27,14 @@ class MovementAction(Action):
 
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return  # Destination is out of bounds.
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return  # Destination is blocked by a tile.
+
+        entity.move(self.dx, self.dy)
